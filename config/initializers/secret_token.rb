1fe2002d174b4013dd859d4915159890ed74f31e
Mark Bennett <mark@burmis.ca>
2012-11-11 16:40:14 -0700

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YegrbCom::Application.config.secret_token = 'b6c8967b44cb9a22dc7b7878a347fa60f526fa542e68f92588fa303d99567d0bfdf164344631200fff016a374338ba12e795e6900543e6441aad0074600ff276'
