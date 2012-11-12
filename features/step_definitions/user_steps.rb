81375870c6a87cc48f5f001284a380fdffa2b814
Mark Bennett <mark@burmis.ca>
2012-11-12 16:26:03 -0700

    We're logging in using our tests.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,4 @@
 Given /^a valid user$/ do
-  pending
   @user = User.create!({
              :email => "batman@wayne.com",
              :password => "12345678",
@@ -8,9 +7,8 @@ Given /^a valid user$/ do
 end
 
 Given /^a logged in user$/ do
-  pending
-  Given "a valid user"
-  visit signin_url
+  step "a valid user"
+  visit new_user_session_url
   fill_in "Email", :with => "batman@wayne.com"
   fill_in "Password", :with => "12345678"
   click_button "Sign in"
