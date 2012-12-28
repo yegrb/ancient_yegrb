ad80510af5ec0a1276646119f62c752b81f012b9
Mark Bennett <mark@burmis.ca>
2012-12-28 14:33:45 -0700

    Add yeoman angular app.

---
+++ client/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var clientApp = angular.module('clientApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+clientApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
