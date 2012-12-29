baf7525af03799830df2201ffcbd3df3ee63ce79
Mark Bennett <mark@burmis.ca>
2012-12-29 15:27:59 -0700

    MainCtrl sets the nextEvent

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 clientApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+  $scope.nextEvent = {
+    name: "Meetup",
+    description: "Join us this month for talks on integrating AngularJS with Rails, RubyFlux, and the new Reel server.",
+    startsOn: new Date(2012, 12, 29, 18, 30),
+    venueAddress: "1234 Not Real Street",
+    venueUrl: "http://ualberta.net",
+    rsvpUrl: "http://plus.com"
+  };
 });
