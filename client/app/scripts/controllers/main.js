6438b2049895324b236d63ddd7a5856a71ff9da9
Mark Bennett <mark@burmis.ca>
2012-12-30 12:40:59 -0700

    Start creating an Events service.
    Currently the focus is on defining how MainCtrl will use it, though the
    next step will be stubbing it out in the actual service implementation.

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,12 +1,7 @@
 'use strict';
 
-clientApp.controller('MainCtrl', function($scope) {
-  $scope.nextEvent = {
-    name: "Meetup",
-    description: "Join us this month for talks on integrating AngularJS with Rails, RubyFlux, and the new Reel server.",
-    startsOn: new Date(2012, 12, 29, 18, 30),
-    venueAddress: "1234 Not Real Street",
-    venueUrl: "http://ualberta.net",
-    rsvpUrl: "http://plus.com"
-  };
+clientApp.controller('MainCtrl', function($scope, events) {
+  events.requestNextEvent().then(function(next_event) {
+    $scope.nextEvent = next_event;
+  });
 });
--- client/test/spec/controllers/main.js
+++ client/test/spec/controllers/main.js
@@ -6,17 +6,70 @@ describe('Controller: MainCtrl', function() {
   beforeEach(module('clientApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    rootScope,
+    mockEvents;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller) {
-    scope = {};
+  beforeEach(inject(function($rootScope, $controller, $q) {
+    var deferred;
+
+    rootScope = $rootScope;
+    scope = $rootScope.$new();
+
+    deferred = $q.defer();
+
+    mockEvents = {
+      requestNextEvent: function() {
+        var self = this;
+
+        deferred.promise.then(function() {
+          self.resolved = true;
+        });
+
+        deferred.resolve({
+          name: "Meetup",
+          description: "Join us this month for talks on integrating AngularJS with Rails, RubyFlux, and the new Reel server.",
+          startsOn: new Date(2012, 12, 29, 18, 30),
+          venueAddress: "1234 Not Real Street",
+          venueUrl: "http://ualberta.net",
+          rsvpUrl: "http://plus.com"
+        });
+
+        return deferred.promise;
+      }
+    };
+    spyOn(mockEvents, 'requestNextEvent').andCallThrough();
+
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
+      events: mockEvents
     });
   }));
 
+  it('should request the next event', function() {
+    expect(mockEvents.requestNextEvent).toHaveBeenCalled();
+  });
+
   it('should attach the nextEvent to the scope', function() {
-    expect(scope.nextEvent).toBeDefined();
+    // GOT IT!!
+    //
+    // The $q service comes from the $QProvider. This provider creates a new
+    // $q service using the qFactory(nextTick, exceptionHandler) signature.
+    // The default nextTick function is what registered work to happen. By
+    // default it registers work using $rootScope.$evalAsync which isn't called
+    // until after the next digest happens.
+    //
+    // Therefore to make this test pass I just needed to digest the $rootScope
+    // and voila. Magic! :)
+    rootScope.$digest();
+
+    waitsFor(function() {
+      return mockEvents.resolved;
+    }, 'requestNextEvent did not resolve', 5000);
+
+    runs(function() {
+      expect(scope.nextEvent).toBeDefined();
+    });
   });
 });
