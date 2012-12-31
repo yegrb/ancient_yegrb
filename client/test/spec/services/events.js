dca74c97324fa272492ce8b43192f61f1bc9052c
Mark Bennett <mark@burmis.ca>
2012-12-31 10:41:47 -0700

    Events service requestNextEvent returns event.
    As this is a new API the implementation doesn't actually reach out to
    the server yet, though eventually it will.

--- client/test/spec/services/events.js
+++ client/test/spec/services/events.js
@@ -6,16 +6,24 @@ describe('Service: events', function () {
   beforeEach(module('clientApp'));
 
   // instantiate service
-  var events;
-  beforeEach(inject(function(_events_) {
+  var events, rootScope;
+
+  beforeEach(inject(function(_events_, $rootScope) {
     events = _events_;
+    rootScope = $rootScope;
   }));
 
   describe('requestNextEvent', function() {
     it('returns a promise that is resolved with the value of the next event', function () {
-      expect(events.requestNextEvent()).toBeDefined();
+      var next_event;
+      
+      events.requestNextEvent().then(function(e) {
+        next_event = e;
+      });
+
+      rootScope.$digest();
+
+      expect(next_event).toBeDefined();
     });
   });
-
-
 });
