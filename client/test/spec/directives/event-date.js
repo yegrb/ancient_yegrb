5cbd759893dcec16abd7e3e43fd5438104001929
Mark Bennett <mark@burmis.ca>
2013-01-02 20:12:20 -0700

    Get that directive test working.

--- client/test/spec/directives/event-date.js
+++ client/test/spec/directives/event-date.js
@@ -6,8 +6,14 @@ describe('Directive: eventDate', function() {
   var element;
 
   it('should make hidden element visible', inject(function($rootScope, $compile) {
-    element = angular.element('<event-date></event-date>');
+    element = angular.element('<event-date time="time"></event-date>');
+
+    $rootScope.time = new Date(2013, 0, 1, 18, 30);
+
     element = $compile(element)($rootScope);
-    expect(element.text()).toBe('this is the eventDate directive');
+
+    $rootScope.$digest();
+
+    expect(element.html()).toBe('<div class="date"><span class="weekday ng-binding">Tuesday</span> <span class="day ng-binding">01</span> <span class="month ng-binding">Jan</span></div>');
   }));
 });
