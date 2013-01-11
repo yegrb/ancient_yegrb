437188ecb0b2191557df46e74eb8649015a9e63f
Mark Bennett <mark@burmis.ca>
2013-01-11 16:58:06 -0700

    Not going to be implementing this now. Focusing on client.

--- spec/models/events_ical_builder_spec.rb
+++ spec/models/events_ical_builder_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe EventsIcalBuilder do
 	it "should generate an empty iCal given no events" do
+    pending
 		EventsIcalBuilder.events_to_ical([]).should eql ""
 	end
 end
